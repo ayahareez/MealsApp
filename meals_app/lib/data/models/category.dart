c0e9ac411c82d75d65bbb0e2504f9df73ddd50d2
ayahareez <lya@gmail.com>
2023-09-21 21:51:18 +0300

    'first'

--- meals_app/lib/data/models/category.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-class CategorySection{
-  final String categoryName;
-  final Color color;
-  final int id;
-  CategorySection({required this.color,required this.categoryName,required this.id});
-}
--- meals_app/lib/data/models/meals.dart
+++
@@ -1,5 +0,0 @@
-class Meal{
-  final String name,time,standard,difficulty,imageUrl,ingredients,steps;
-  final int id;
-  Meal({required this.name,required this.time,required this.standard,required this.difficulty,required this.imageUrl,required this.id,required this.steps,required this.ingredients});
-}
